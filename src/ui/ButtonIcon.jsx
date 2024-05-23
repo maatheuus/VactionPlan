46b55447495f4ffbc5fb57d00279ebbdcab928cb
MATHEUS <matts14smkd@gmail.com>
2024-05-23 14:40:31 -0300

    adicionando novas funcionalidades

--- src/ui/ButtonIcon.jsx
+++ src/ui/ButtonIcon.jsx
@@ -1,17 +1,27 @@
 import { Link } from "react-router-dom";
+import { twMerge } from "tailwind-merge";
 
-function ButtonIcon({ label, to, children, icon, ...props }) {
-  if (to) {
+function ButtonIcon({ to, type, className, children, ...props }) {
+  const base =
+    "flex flex-1 justify-center gap-2 order-first rounded-md bg-gray-200 px-3 py-1.5 text-sm font-semibold leading-6 text-black hover:bg-gray-300";
+
+  const styles = {
+    primary: base,
+    secondary:
+      base +
+      " flex-0 justify-start w-full bg-transparent text-zinc-300 hover:text-zinc-50 hover:bg-transparent transition-all px-0",
+  };
+
+  if (to)
     return (
-      <Link to={to} className="button-icon">
+      <Link to={to} className={twMerge(styles[type], className)} {...props}>
         {children}
       </Link>
     );
-  }
+
   return (
-    <button className="button-icon" {...props}>
-      {icon}
-      {label && <span>{label}</span>}
+    <button className={twMerge(styles[type], className)} {...props}>
+      {children}
     </button>
   );
 }
