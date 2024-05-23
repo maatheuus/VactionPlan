6a5388cf436e9564ab356e9dad9c904895d66c12
MATHEUS <matts14smkd@gmail.com>
2024-05-23 14:54:50 -0300

    adicionando nova estilização

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,15 @@
 import { Outlet } from "react-router-dom";
 
+import SideBar from "../ui/SideBar";
+import Header from "../ui/Header";
+
 function AppLayout() {
   return (
-    <div className="app-layout">
-      <main className="main">
-        <div className="app-layout__container">
+    <div className="flex h-screen bg-zinc-950 p-2">
+      <SideBar />
+      <main className="flex-1 bg-zinc-50 rounded-xl overflow-y-scroll">
+        <Header />
+        <div className="max-w-7xl my-0 mx-auto flex flex-col gap-10 py-10 px-4 small:p-10">
           <Outlet />
         </div>
       </main>
