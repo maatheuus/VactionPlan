84ca9312b13644050c2ae939c9186f7e58d6535d
MATHEUS <matts14smkd@gmail.com>
2024-05-23 15:05:25 -0300

    adicionando nova funcionalidade

--- src/features/requests/NewRequests.jsx
+++ src/features/requests/NewRequests.jsx
@@ -1,32 +1,19 @@
 import { useForm } from "react-hook-form";
-import { useMoveTo } from "../../hooks/useMoveTo";
-import { useCreateRequest } from "../requests/useCreateRequest";
-import { motion } from "framer-motion";
-
-import {
-  FaCircleArrowLeft,
-  FaLocationDot,
-  FaRegUser,
-  FaHourglassStart,
-  FaHourglassEnd,
-} from "react-icons/fa6";
-
-import { handleErrorsMessages } from "../../services/toastApi";
-
-import Button from "../../ui/Button";
-import SpinnerMini from "../../ui/SpinnerMini";
-
-import { useAnimatePages } from "../../hooks/useAnimatePages";
-
+import { Loader } from "lucide-react";
 import {
   formattingDistance,
   checkDistance,
 } from "../../services/formattingDate";
+import { useMoveTo } from "../../hooks/useMoveTo";
+import { useCreateRequest } from "../requests/useCreateRequest";
+import { handleErrorsMessages } from "../../services/toastApi";
+
+import Button from "../../ui/Button";
+import FormRow from "../../ui/FormRow";
 
 function NewRequests() {
   const { setTo } = useMoveTo();
   const { isCreating, createRequest } = useCreateRequest();
-  const { variants, initial, animate, exit } = useAnimatePages();
 
   const {
     register,
@@ -47,125 +34,141 @@ function NewRequests() {
     else if (checkDate)
       handleErrorsMessages("The date needs greater than the selected date");
     else {
-      createRequest(data);
-      if (!isCreating) {
-        setTo("/requests");
-      }
+      createRequest(data, {
+        onSuccess: () => setTo("/requests"),
+      });
     }
   };
+
+  const variation = {
+    base: "block w-full rounded-md border-0 pl-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-zinc-400 focus:ring-1 focus:ring-inset focus:ring-zinc-500 sm:text-sm sm:leading-6",
+    user: "rounded-md border-0 py-1.5 px-3 ring-1 ring-inset ring-gray-300",
+  };
+
   return (
-    <section id="employee">
-      <Button onClick={() => setTo(-1)} className="button-all">
-        <FaCircleArrowLeft className="arrow-left" />
-      </Button>
-
-      <motion.div
-        className="screen-employee"
-        variants={variants}
-        initial={initial}
-        animate={animate}
-        exit={exit}
+    <>
+      <div>
+        <h1 className="text-2xl font-semibold leading-7 text-gray-900 mb-3">
+          Create new Request
+        </h1>
+      </div>
+      <form
+        className="shadow py-8 px-16 overflow-hidden text-lg rounded-lg"
+        onSubmit={handleSubmit(onSubmit)}
       >
-        <h1 className="screen-employee__title">Requests for vacations</h1>
+        <FormRow label="Full name" error={errors?.userName?.message}>
+          <input
+            type="text"
+            id="userName"
+            className={variation["user"]}
+            disabled={isCreating}
+            {...register("userName", {
+              required: {
+                value: true,
+                message: "Provide you name.",
+              },
+              validate: (value) =>
+                value.trim() === "" ? "Provide a valid name." : null,
+            })}
+          />
+        </FormRow>
+        <FormRow label="Email address" error={errors?.email?.message}>
+          <input
+            type="email"
+            id="email"
+            className={variation["user"]}
+            disabled={isCreating}
+            {...register("email", {
+              required: "This field is required",
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "Please provide a valid email address",
+              },
+            })}
+          />
+        </FormRow>
 
-        <form className="form">
-          <div className="form__inputs">
-            <div className="form__inputs--col1">
-              <div className="form-group">
-                <FaRegUser className="form__icon" />
-                <input
-                  placeholder="Your name"
-                  className="form__input"
-                  type="text"
-                  {...register("userName", {
-                    required: {
-                      value: true,
-                      message: "Provide you name.",
-                    },
-                    validate: (value) =>
-                      value.trim() === "" ? "Provide a valid name." : null,
-                  })}
-                />
-                <p className="error-inputs">{errors?.userName?.message}</p>
-              </div>
+        <FormRow label="Location" error={errors?.location?.message}>
+          <input
+            type="text"
+            id="location"
+            className={variation["user"]}
+            disabled={isCreating}
+            {...register("location", {
+              required: {
+                value: true,
+                message: "Provide your location.",
+              },
+              validate: (value) =>
+                value.trim() === "" ? "Provide a valid location." : null,
+            })}
+          />
+        </FormRow>
 
-              <div className="form-group">
-                <FaLocationDot className="form__icon" />
-                <input
-                  placeholder="Location"
-                  className="form__input"
-                  type="text"
-                  {...register("location", {
-                    required: {
-                      value: true,
-                      message: "Provide your location.",
-                    },
-                    validate: (value) =>
-                      value.trim() === "" ? "Provide a valid location." : null,
-                  })}
-                />
-                <p className="error-inputs">{errors?.location?.message}</p>
-              </div>
+        <FormRow label="Date start" error={errors?.startDate?.message}>
+          <input
+            id="startDate"
+            type="date"
+            className={variation["user"]}
+            disabled={isCreating}
+            {...register("startDate", {
+              valueAsDate: true,
+              required: {
+                value: true,
+                message: "Provide the start date.",
+              },
+            })}
+          />
+        </FormRow>
+        <FormRow label="Date end" error={errors?.endDate?.message}>
+          <input
+            id="endDate"
+            type="date"
+            className={variation["user"]}
+            disabled={isCreating}
+            {...register("endDate", {
+              valueAsDate: true,
+              required: {
+                value: true,
+                message: "Provide the end date.",
+              },
+            })}
+          />
+        </FormRow>
 
-              <div className="form-group">
-                <textarea
-                  className="form__input textarea"
-                  rows="4"
-                  placeholder="Observations"
-                  type="text"
-                  {...register("observation")}
-                />
-              </div>
-            </div>
+        <FormRow label="Observation">
+          <textarea
+            name="observation"
+            id="observation"
+            className={variation["user"]}
+            disabled={isCreating}
+            {...register("observation")}
+            rows={4}
+            cols={40}
+            style={{
+              height: "50px",
+            }}
+          />
+        </FormRow>
 
-            <div className="form__inputs--col2">
-              <div className="form-group">
-                <FaHourglassStart className="form__icon" />
-
-                <input
-                  className="form__input"
-                  type="date"
-                  {...register("startDate", {
-                    valueAsDate: true,
-                    required: {
-                      value: true,
-                      message: "Provide the start date.",
-                    },
-                  })}
-                />
-                <p className="error-inputs">{errors?.startDate?.message}</p>
-              </div>
-
-              <div className="form-group">
-                <FaHourglassEnd className="form__icon" />
-                <input
-                  className="form__input"
-                  type="date"
-                  {...register("endDate", {
-                    valueAsDate: true,
-                    required: {
-                      value: true,
-                      message: "Provide the end date.",
-                    },
-                  })}
-                />
-                <p className="error-inputs">{errors?.endDate?.message}</p>
-              </div>
-
-              <div className="form__button">
-                <Button
-                  onClick={() => handleSubmit(onSubmit)()}
-                  disabled={isCreating}
-                  className="btn-send"
-                >
-                  {isCreating ? <SpinnerMini /> : "Send request"}
-                </Button>
-              </div>
-            </div>
-          </div>
-        </form>
-      </motion.div>
-    </section>
+        <div className="mt-6 flex flex-col gap-y-6 gap-x-6 items-center small:grid small:grid-cols-3 small:gap-y-0">
+          <Button
+            variation="secondary"
+            type="reset"
+            className="col-start-1 text-sm font-semibold leading-6 text-gray-900 border-2 border-gray-300 w-full small:w-auto sm:col-start-2"
+            disabled={isCreating}
+          >
+            Cancel
+          </Button>
+          <Button
+            variation="primary"
+            className="col-start-3 -order-1 w-full small:order-none small:w-auto"
+          >
+            {isCreating ? <Loader className="animate-spin" /> : "Save"}
+          </Button>
+        </div>
+      </form>
+    </>
   );
 }
 
